05ed342b3c6e72bd7fee4d77812ec5bd6081d315
Mark A. <mark001616@gmail.com>
2024-09-15 12:04:53 -0400

    Add CSS support

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 const PORT = 3000;
 
 const indexRouter = require("./routes/indexRouter");
